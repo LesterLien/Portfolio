fb52d08a91e9cf7fc14123fe0a85763522a1bf9a
LesterLien <lesterlien11@gmail.com>
2025-05-16 22:51:28 -0700

    Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {},
+};
