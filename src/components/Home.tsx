3385b8904ab45d16b44340a91172a865fbbb9639
LesterLien <lesterlien11@gmail.com>
2025-05-20 03:29:07 -0700

    Setup more home layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar";
-
+import Footer from "../components/Footer";
 function Home() {
 
   return (
     <div className="min-h-screen">
-          <Navbar/>
+        <Navbar/>
         <div className="flex flex-col justify-center items-center m-3">
-          <div className="font-bold text-white text-4xl m-3">
+          <div className="font-bold text-white text-4xl m-3" id="about">
             <h1>About Me</h1>
           </div>
           <img 
@@ -14,13 +14,16 @@ function Home() {
             alt="UCR" 
             className="w-auto h-100 rounded-xl outline-1 outline-white"
           />
-          <div className="font-bold text-white text-4xl m-3">
+          <div className="font-bold text-white text-4xl m-3" id="skills">
             <h1>Skills</h1>
+            <div className="flex flex-row justify-center items-center m-3">
+              
+            </div>
           </div>
-          <div className="font-bold text-white text-4xl m-3 flex flex-col justify-center items-center">
+          <div className="font-bold text-white text-4xl m-3 flex flex-col justify-center items-center" id="projects">
             <h1>Projects</h1>
-            <div className="flex flex-row justify-center items-center m-3 ">
-                <div className="m-3 w-85 h-120 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out hover:-translate-y-2">
+            <div className="flex flex-row justify-center items-center m-3">
+                <div className="m-3 w-85 h-120 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2">
                   Project 1
                 </div>
                  <div className="m-3 w-85 h-120 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out hover:-translate-y-2">
@@ -33,6 +36,7 @@ function Home() {
             <a>See More</a>
           </div>
         </div>
+        <Footer/>
     </div>
   );
 }
