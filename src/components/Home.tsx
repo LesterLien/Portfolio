a156e541c21262b6292bd76b46bd3dc6319e5341
LesterLien <lesterlien11@gmail.com>
2025-05-25 04:44:28 -0700

    Added more styling to the skills

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -30,25 +30,24 @@ function Home() {
             className="w-full max-w-md sm:max-w-lg md:max-w-xl rounded-xl outline-1 outline-white"
           />
           
-          <div className="text-white text-4xl m-3 flex flex-col justify-center items-center" id="skills">
-            <h1 className="font-bold">Skills</h1>
+          <div className="text-4xl m-3 flex flex-col justify-center items-center" id="skills">
+            <h1 className="font-bold text-white">Skills</h1>
             <div className="flex flex-wrap justify-center items-start m-3">
               {skills.map((skillCategory, index) => (
                 <div
                   key={index}
-                  className="m-3 w-80 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
+                  className="m-3 w-80 min-h-70 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
                 >
-                  <h2 className="text-xl font-semibold mb-3 flex justify-center items-center">{skillCategory.title}</h2>
-                  <div className="flex flex-wrap justify-center items-center gap-3">
+                  <h2 className="text-xl font-semibold mb-3 flex justify-center items-center text-white">{skillCategory.title}</h2>
+                  <div className="flex flex-wrap justify-center items-center gap-3 text-[#84858A]">
                     {skillCategory.skills.map((item, index) => {
                       const Icon = item.image;
                       return (
                         <div key={index} className="flex flex-row items-center w-auto border-1 rounded-lg p-3">
-                          <Icon className="w-[24px] h-[24px] mr-2" />
-
-                          <span className="text-[16px]">
+                          <Icon className="w-[24px] h-[24px] mr-2 text-[#854CE6]"/>
+                          <span className="text-[16px] text-white">
                             {item.name}
-                            </span>
+                          </span>
                         </div>
                       );
                     })}
