63620ff445f619000c0707becd1d8ff0677a7789
LesterLien <lesterlien11@gmail.com>
2025-05-25 02:16:43 -0700

    Added About Me description

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,13 +6,25 @@ function Home() {
     <div className="min-h-screen">
         <Navbar/>
         <div className="flex flex-col justify-center items-center m-3">
-          <div className="font-bold text-white text-4xl m-3" id="about">
-            <h1>About Me</h1>
+          <div className="text-white text-4xl m-3 flex flex-wrap flex-col justify-center items-center" id="about">
+            <h1 className="font-bold">
+              About Me
+            </h1>
+            <div className="text-white text-[16px] max-w-3xl text-center m-4">
+                <p>
+                  Hi, I am Lester Lien. 
+                  I recently graduated from the University of California, Riverside with a Bachelor of Science in Computer Science. 
+                  I aspire to be a software engineer and enjoy working on full-stack projects. 
+                  I have been learning more about IT support and how it operates.
+                  At the moment, I am seeking opportunities to improve my skills, work alongside a team, and contribute towards something intriguing. 
+                  Whenever I am coding web apps or providing technical support, I am constantly eager to learn and perform well.
+                </p>
+            </div>
           </div>
           <img 
             src="/assets/images/UCR.JPG" 
             alt="UCR" 
-            className="w-auto h-100 rounded-xl outline-1 outline-white"
+            className="w-full max-w-md sm:max-w-lg md:max-w-xl rounded-xl outline-1 outline-white"
           />
           <div className="font-bold text-white text-4xl m-3" id="skills">
             <h1>Skills</h1>
@@ -22,7 +34,7 @@ function Home() {
           </div>
           <div className="font-bold text-white text-4xl m-3 flex flex-col justify-center items-center" id="projects">
             <h1>Projects</h1>
-            <div className="flex flex-row justify-center items-center m-3">
+            <div className="flex flex-wrap justify-center items-center m-3">
                 <div className="m-3 w-85 h-120 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2">
                   Project 1
                 </div>
