f57673bc07438d32c4ff132dce680fce0ea139b5
LesterLien <lesterlien11@gmail.com>
2025-05-27 02:32:20 -0700

    Added routing back to home

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -4,11 +4,13 @@ export default function Navbar() {
   return (
     <div className="h-20 bg-[#191923] grid grid-cols-3 items-center outline-1 outline-white">
       <div className="flex justify-start ml-5">
-        <img 
-          src="/assets/images/logo.webp" 
-          alt="logo" 
-          className="w-auto h-20"
-        />
+        <Link to="/" className="cursor-hover">
+          <img 
+            src="/assets/images/logo.webp" 
+            alt="logo" 
+            className="w-auto h-20 duration-300 ease-in-out hover:scale-125"
+          />
+        </Link>
       </div>
       <div className="flex justify-center space-x-8 text-white text-[20px] m-4">
         <Link to="/projects" className="hover:text-[#854CE6] transition-colors duration-200">Projects</Link>
