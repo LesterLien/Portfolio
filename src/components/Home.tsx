1651b1fd63d09f5a885ac097e5aa8a4f78b30a36
LesterLien <lesterlien11@gmail.com>
2025-05-28 23:47:41 -0700

    Updated projects UI

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -80,17 +80,17 @@ function Home() {
               Projects
             </h1>
             <div className="flex flex-wrap justify-center items-center m-3 cursor-hover">
-              {projects.map((projects, index) => (
+              {projects.slice(0,3).map((projects, index) => (
                 <div
                   key={index}
-                  className="m-3 w-95 h-65 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
+                  className="m-3 w-95 h-68 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
                 >
                   <div className="flex flex-col justify-center text-[#C3C4C5]">
-                    <div className="flex flex-row justify-between">
-                      <h2 className="text-[24px] mb-2">{projects.title}</h2>
-                      <h3 className="text-[14px] mt-2 text-[#55555B] font-normal">{projects.date}</h3>
+                    <div className="flex flex-col">
+                      <h2 className="text-[24px] mb-1">{projects.title}</h2>
+                      <h3 className="text-[14px] mb-2 text-[#55555B] font-normal">{projects.date}</h3>
                     </div>
-                    <div className="mb-3">
+                    <div className="mb-3 overflow-hidden h-[171px] w-[340px]">
                       <img src={projects.image} className="rounded-lg"/>
                     </div>
                   </div>
