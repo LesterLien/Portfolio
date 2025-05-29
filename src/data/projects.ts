755207eec657d40a8238ddc5fd23010057aeb10f
LesterLien <lesterlien11@gmail.com>
2025-05-28 23:28:43 -0700

    Updated projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
         id: 0,
         title: "PixelHive",
         date: "Feb 2025 - Present",
-        image: "/assets/images/projects/PixelHive.PNG",
+        image: "/assets/images/projects/PixelHive.png",
         tags: [
             "JavaScript",
             "React", 
@@ -15,13 +15,14 @@ export const projects = [
             "SQLite3", 
             "Steam API", 
             "IsThereAnyDeal API"
-        ]
+        ],
+        github: "https://github.com/LesterLien/PixelHive"
     }, 
     {
         id: 1,
         title: "AniLytics",
         date: "April 2025 - Present",
-        image: "/assets/images/projects/AniLytics.PNG",
+        image: "/assets/images/projects/AniLytics.png",
         tags: [
             "TypeScript", 
             "React", 
@@ -30,13 +31,14 @@ export const projects = [
             "Express.js", 
             "PostgreSQL"
 
-        ]
+        ],
+        github: "https://github.com/LesterLien/AniLytics"
     },
     {
         id: 2,
         title: "Boba Thoughts",
         date: "Jan 2024 - Mar 2024",
-        image: "/assets/images/projects/BobaThoughts.PNG",
+        image: "/assets/images/projects/BobaThoughts.png",
         tags: [
             "Javascript", 
             "React", 
@@ -48,7 +50,68 @@ export const projects = [
             "Firebase", 
             "Yelp API", 
             "Google API"
-        ]
+        ],
+        github: "https://github.com/stormyy00/BobaThoughts"
+    },
+    {
+        id: 3,
+        title: "Resume 322",
+        date: "Sept 2023 - Mar 2024",
+        image: "/assets/images/projects/Resume322.png",
+        tags: [
+            "JavaScript", 
+            "React", 
+            "Node.js", 
+            "Express.js", 
+            "MongoDB", 
+            "TailwindCSS", 
+            "CSS",
+        ],
+        github: "https://github.com/UCR-Senior-Design/course-project-dvl"
+    },
+    {
+        id: 4,
+        title: "Conscious Cart",
+        date: "Apr 2024",
+        image: "/assets/images/projects/ConsciousCart.png",
+        tags: [
+            "Flutter", 
+            "Dart", 
+            "Android Studio", 
+            "Gemini API", 
+            "FDC API", 
+            "Hackathon"
+        ],
+        github: "https://github.com/TheFiveHeads/Google-AI-Hackathon-Project"
+    },
+    {
+        id: 5,
+        title: "Game Rental",
+        date: "Apr 2024 - June 2024",
+        image: "/assets/images/projects/ConsciousCart.png",
+        tags: [
+            "JavaScript", 
+            "React", 
+            "Next.js", 
+            "PostgreSQL", 
+            "TailwindCSS", 
+            "Java"
+        ],
+        github: "https://github.com/stormyy00/cs166", 
+        github2: "https://github.com/stormyy00/cs166-frontend"
+    },
+    {
+        id: 6,
+        title: "Monster RPG Game",
+        date: "Sept 2022 - Dec 2023",
+        image: "/assets/images/projects/MonsterRPGGame.png",
+        tags: [
+            "C++",
+            "Google Test",
+            "YAML",
+            "UML Diagram",
+        ],
+        github: "https://github.com/LesterLien/Monster-RPG-Game" 
     },
 
 ];
