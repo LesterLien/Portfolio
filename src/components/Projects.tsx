1cbec3416b970709d2dc7a52960099df02cb8aa3
LesterLien <lesterlien11@gmail.com>
2025-05-29 00:08:38 -0700

    Update Projects UI

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,15 +2,15 @@ import { projects } from "../data/projects";
 
 export default function Projects() {
   return (
-    <div className="font-bold text-white text-4xl m-3 flex flex-col justify-center items-center" id="projects">
+    <div className="font-bold text-white text-4xl m-3 flex flex-col justify-center items-center">
       <h1>
         Projects
       </h1>
-      <div className="flex flex-wrap justify-center items-center m-3 cursor-hover">
+      <div className="flex flex-wrap justify-center items-center m-3 gap-6 cursor-hover w-5/6">
         {projects.map((projects, index) => (
           <div
             key={index}
-            className="m-3 w-95 h-68 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col gap-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
+            className="w-95 h-68 bg-[#181823] outline-1 outline-[#181823] rounded-lg shadow-lg shadow-black px-5 py-6 flex flex-col m-3 transition-all duration-500 ease-in-out  hover:-translate-y-2"
           >
             <div className="flex flex-col justify-center text-[#C3C4C5]">
               <div className="flex flex-col">
