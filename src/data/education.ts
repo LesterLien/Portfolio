1b2d782c0f64cea001dcf44155ff24cf1ab48de9
LesterLien <lesterlien11@gmail.com>
2025-05-30 21:41:21 -0700

    Added education data

---
+++ src/data/education.ts
@@ -0,0 +1,22 @@
+export const education = [
+    {
+      id: 0,
+      img: " ",
+      school: "University of California, Riverside",
+      date: "Sept 2022 - Dec 2024",
+      grade: "3.4 GPA",
+      desc: "Completed my Bachelor of Science in Computer Science after transferring from community college.",
+      degree: "Bachelor of Science, Computer Science",
+    },
+    {
+      id: 1,
+      img: " ",
+      school: "Pasadena City College",
+      date: "Aug 2020 - May 2022, July 2023 - Aug 2023, Jun 2024 - Aug 2024",
+      grade: "3.88 GPA",
+      desc: "Completed two years before transferring, then returned to earn Full IGETC Certification for the UC system and additional associate degrees.",
+      degree: "Associate of Arts, Engineering and Technology",
+      degree2: "Associate of Science, Natural Sciences",
+      degree3: "Associate of Science, Applied Science"
+    }
+];
